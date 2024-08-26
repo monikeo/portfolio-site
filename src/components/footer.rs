635b86255193e44b959a15e02a6aeed535130a24
monikeo281000 <dark.moni.281000@gmail.com>
2024-08-26 23:27:12 +0700

    finished basic setup

---
+++ src/components/footer.rs
@@ -0,0 +1,11 @@
+
+use leptos::*;
+
+#[component]
+pub fn Footer() -> impl IntoView {
+    view! {
+        <footer>
+            <h1> Footer </h1>
+        </footer>
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use leptos::*;
+use leptos_router::*;
+
+mod components;
+use components::{
+    header::Header,
+    footer::Footer,
+};
+
+
 fn main() {
-    println!("Hello, world!");
+    mount_to_body(|| view! { <App/> })
+}
+
+#[component]
+fn App() -> impl IntoView {
+    view! {
+        <div>
+            <Header/>
+            /*
+            <Router>
+                <Route path="/" component={Home}/>
+                <Route path="/about" component={About}/>
+                <Route path="/projects" component={Projects}/>
+                <Route path="/contact" component={Contact}/>
+            </Router>
+            */
+            <Footer/>
+        </div>
+    }
 }
