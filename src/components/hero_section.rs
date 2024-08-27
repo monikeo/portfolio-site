012db90fa2a264a97e9b11e40eacfd22a6ae7073
monikeo281000 <dark.moni.281000@gmail.com>
2024-08-27 16:20:24 +0700

    testing hero1 image

--- src/components/hero_section.rs
+++ src/components/hero_section.rs
@@ -6,11 +6,12 @@ pub fn Hero1() -> impl IntoView {
     let hero1_title = "I am Moni";
     let hero1_content = "I am Moni Keo, a dedicated Cyber Security major with a strong commitment  to discipline and continuous learning. As a Taekwondo national athlete, I bring resilience and  focus to everything I do. I love exploring technology, contributing to  open-source projects, and sharing my knowledge through blogging. My  passion for self-improvement and community engagement drives me to  constantly expand my skills and make a positive impact in the tech  world.";
     let hero1_button = "Get resume";
+    let hero1_img = "./../public/assets/images/hero1_profile.jpg";
     view!{
         <div class="hero bg-base-200 min-h-screen">
             <div class="hero-content flex-col lg:flex-row-reverse">
                 <img
-                    src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
+                    src={hero1_img}
                     class="max-w-sm rounded-lg shadow-2xl" />
                 <div>
                     <h1 class="text-5xl font-bold">
