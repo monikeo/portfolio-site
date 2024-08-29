e26813271f952cb7c4958a51316c6f49a347a7ef
monikeo281000 <dark.moni.281000@gmail.com>
2024-08-29 14:35:03 +0700

    add image to homepage

--- src/components/hero_section.rs
+++ src/components/hero_section.rs
@@ -11,8 +11,8 @@ pub fn Hero1() -> impl IntoView {
     let hero1_content = "I am Moni Keo, a dedicated Cyber Security major with a strong commitment  to discipline and continuous learning. As a Taekwondo national athlete, I bring resilience and  focus to everything I do. I love exploring technology, contributing to  open-source projects, and sharing my knowledge through blogging. My  passion for self-improvement and community engagement drives me to  constantly expand my skills and make a positive impact in the tech  world.";
     let hero1_button1 = "Get resume";
     let hero1_button2 = "About";
-    let hero1_img = "https://drive.google.com/uc?export=view&id=1EBIbrsIZhrHqsD0B7GuLim23k26n0mM3";
-    let hero1_img_alt = "";
+    let hero1_img = "https://i.postimg.cc/cL95xQBV/hero1-profile.jpg";
+    let hero1_img_alt = "Moni's Profile'";
 
     view!{
         <div class="hero bg-base-100 min-h-screen">
