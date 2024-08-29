1d844a0e2521485c577daffc9ab22328abeded37
monikeo281000 <dark.moni.281000@gmail.com>
2024-08-29 14:38:31 +0700

    change education timeline color

--- src/components/portfolio/education.rs
+++ src/components/portfolio/education.rs
@@ -66,7 +66,7 @@ pub fn EducationTimeline() -> impl IntoView {
                         xmlns="http://www.w3.org/2000/svg"
                         viewBox="0 0 20 20"
                         fill="currentColor"
-                        class="h-5 w-5">
+                        class="h-5 w-5 text-secondary">
                             <path
                                 fill-rule="evenodd"
                                 d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
@@ -94,7 +94,7 @@ pub fn EducationTimeline() -> impl IntoView {
                         xmlns="http://www.w3.org/2000/svg"
                         viewBox="0 0 20 20"
                         fill="currentColor"
-                        class="h-5 w-5">
+                        class="h-5 w-5 text-secondary">
                         <path
                             fill-rule="evenodd"
                             d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
@@ -122,7 +122,7 @@ pub fn EducationTimeline() -> impl IntoView {
                         xmlns="http://www.w3.org/2000/svg"
                         viewBox="0 0 20 20"
                         fill="currentColor"
-                        class="h-5 w-5">
+                        class="h-5 w-5 text-secondary">
                         <path
                             fill-rule="evenodd"
                             d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
@@ -150,7 +150,7 @@ pub fn EducationTimeline() -> impl IntoView {
                         xmlns="http://www.w3.org/2000/svg"
                         viewBox="0 0 20 20"
                         fill="currentColor"
-                        class="h-5 w-5">
+                        class="h-5 w-5 text-secondary">
                             <path
                                 fill-rule="evenodd"
                                 d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
@@ -159,7 +159,7 @@ pub fn EducationTimeline() -> impl IntoView {
                 </div>
                 <div class="timeline-start mb-10 md:text-end">
                     <time class="font-mono italic">2015</time>
-                    <div class="text-lg font-black">Apple Watch</div>
+                    <div class="text-lg font-black">School</div>
                     <p>
 
                     </p>
