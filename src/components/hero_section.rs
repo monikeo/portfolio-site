e0144741a7294bcd65164fa2d08511a9af36fdcb
monikeo281000 <dark.moni.281000@gmail.com>
2024-08-29 10:14:41 +0700

    test nerdfont

--- src/components/hero_section.rs
+++ src/components/hero_section.rs
@@ -13,29 +13,40 @@ pub fn Hero1() -> impl IntoView {
     let hero1_button2 = "About";
     let hero1_img = "https://drive.google.com/uc?export=view&id=1EBIbrsIZhrHqsD0B7GuLim23k26n0mM3";
     let hero1_img_alt = "";
+
+    let facebook = "";
+    let github = "";
+    let instagram = "";
+    let linkedin = "󰌻";
     view!{
-        <div class="hero bg-base-200 min-h-screen">
+        <div class="hero bg-base-100 min-h-screen">
             <div class="hero-content flex-col lg:flex-row-reverse">
                 <img
                     src={hero1_img}
                     alt={hero1_img_alt}
                     class="max-w-sm rounded-lg shadow-2xl" />
                 <div>
-                    <h1 class="text-5xl font-bold">
+                    <h1 class="text-5xl font-bold text-display">
                         {hero1_title}
                     </h1>
-                    <p class="py-6 w-4/5">
+                    <p class="py-6 w-4/5 text-display">
                         {hero1_content}
                     </p>
                     <div>
                         <SocialMedia />
                     </div>
                     <div class="flex space-x-8">
-                            <button class="btn btn-outline btn-accent">
+                            <button class="btn btn-outline btn-primary">
                                 {hero1_button1}
                             </button>
-                            <button class="btn btn-outline btn-info">
+                            <button class="btn btn-outline btn-secondary">
                                 {hero1_button2}
+                            </button>
+
+                            <button class="btn btn-outline">
+                            <a class="btn-display">
+                            {facebook}
+                                </a>
                             </button>
                     </div>
                 </div>
