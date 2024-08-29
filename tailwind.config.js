33a27f9bca8d1faa8898901d4fa224dd30fac7ac
monikeo281000 <dark.moni.281000@gmail.com>
2024-08-29 09:19:08 +0700

    change default theme to dark

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,7 @@ module.exports = {
     require('daisyui'),
   ],
   daisyui: {
-    themes: ["fantasy","dark"], // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
+    themes: ["dark", "fantasy"], // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
     darkTheme: "dark", // name of one of the included themes for dark mode
     base: true, // applies background color and foreground color for root element by default
     styled: true, // include daisyUI colors and design decisions for all components
